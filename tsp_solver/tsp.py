d5835b11f0821178e60b1e776ff0ab617f12c31d
mathe0 <matty.szczesny@gmail.com>
2018-06-09 18:06:33 +0200

    final#

--- tsp_solver/tsp.py
+++ tsp_solver/tsp.py
@@ -1,9 +1,9 @@
 from numpy import random
 import matplotlib.pyplot as plt
 import time
-from tsp_utils import generate_routes, calculate_costs, points_for_route
+from tsp_utils import generate_routes, calculate_costs, points_for_route, generate_distances, nna
 
-NUMBER_OF_CITIES = 5
+NUMBER_OF_CITIES = 7
 DIMENSIONS = 2
 MAXIMUM_DISTANCE = 1000
 
@@ -14,33 +14,50 @@ def main():
     for city_number in range(NUMBER_OF_CITIES):
         cities.append([cities_x[city_number], cities_y[city_number]])
 
-    routes = generate_routes(NUMBER_OF_CITIES)
+    brute_force_method(cities)
+    nearest_neighbour_algorithm(cities)
 
-    brute_force_method(cities, routes)
 
+def brute_force_method(cities):
 
-def brute_force_method(cities, routes):
     start_time = time.time()
+    routes = generate_routes(NUMBER_OF_CITIES)
     results = calculate_costs(routes, cities)
     execution_time = time.time() - start_time
 
     best_route = routes[min(results, key=results.get)]
-    print(
-        "### BRUTE FORCE METHOD ###" +
-        "\nroute: " + " -> ".join(str(x) for x in best_route) +
-        "\ndistance: " + str(results[min(results, key=results.get)]) +
-        "\ntime: " + str(execution_time) + " seconds"
-    )
-
+    cost = results[min(results, key=results.get)]
     cities_x, cities_y = points_for_route(best_route, cities)
 
-    plt.title('BRUTE FORCE')
+    display_results('BRUTE FORCE METHOD', best_route, cities_x, cities_y, cost, execution_time)
 
+
+def nearest_neighbour_algorithm(cities):
+    """
+    The nearest neighbour algorithm was one of the first algorithms
+    used to determine a solution to the travelling salesman problem
+    """
+    start_time = time.time()
+    distances = generate_distances(cities, NUMBER_OF_CITIES)
+    best_route, cost = nna(distances, NUMBER_OF_CITIES)
+    execution_time = time.time() - start_time
+    cities_x, cities_y = points_for_route(best_route, cities)
+
+    display_results('NEAREST NEIGHBOUR ALGORITHM', best_route, cities_x, cities_y, cost, execution_time)
+
+
+def display_results(method, best_route, cities_x, cities_y, cost, execution_time):
+    description = str("### " + method + " ###" +
+                      "\nroute: " + " -> ".join(str(x) for x in best_route) +
+                      "\ndistance: " + str(cost) +
+                      "\ntime: " + str(execution_time) + " seconds\n")
+    print(description)
+
+    plt.title(method)
     plt.plot(cities_x, cities_y, 'o', color='gray', markersize=20,
              markerfacecolor='white',
              markeredgecolor='gray',
              markeredgewidth=1)
-
     for i in range(len(best_route) - 1):
         plt.annotate(str(best_route[i]),
                      (cities_x[i], cities_y[i]),
@@ -52,7 +69,6 @@ def brute_force_method(cities, routes):
                      xy=(cities_x[best_route[i]+1], cities_y[best_route[i]+1]), xycoords='data',
                      arrowprops=dict(arrowstyle="->",
                                      connectionstyle="arc3"))
-
     plt.show()
 
 
--- tsp_solver/tsp_utils.py
+++ tsp_solver/tsp_utils.py
@@ -1,19 +1,22 @@
 import itertools
 import math
+import numpy as np
+import numpy.ma as ma
 
 
 def count_cost(p1, p2):
-    dx = p1[0] - p2[0]
-    dy = p1[1] - p2[1]
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
     return math.hypot(dx, dy)
 
 
 def generate_routes(length):
-    lang = [x for x in range(length)]
+    lang = [x for x in range(1, length)]
     routes = list(map(list, itertools.permutations(lang)))
 
     # insert starting point as a finish point
     for route in routes:
+        route.insert(0, 0)
         route.insert(len(route), route[0])
     return routes
 
@@ -22,7 +25,7 @@ def calculate_costs(routes, nodes):
     costs = {}
     for route in routes:
         travel_cost = 0
-        for i in range(len(route) - 2):
+        for i in range(len(route) - 1):
             move_cost = count_cost(nodes[route[i]], nodes[route[i+1]])
             travel_cost += move_cost
         costs[routes.index(route)] = travel_cost
@@ -39,3 +42,33 @@ def points_for_route(route, cities):
         y.append(cities[node][1])
 
     return x, y
+
+
+def generate_distances(cities, number_of_cities):
+    distances = np.zeros(shape=(number_of_cities, number_of_cities), dtype=np.float)
+    for i in range(number_of_cities):
+        for j in range(number_of_cities):
+            distances[i, j] = count_cost(cities[i], cities[j])
+
+    return distances
+
+
+def nna(distances, number_of_cities):
+    route = [0]
+    cost = 0
+
+    mask = np.zeros(number_of_cities, dtype=bool)
+    mask[route] = True
+    distances = np.array(distances)
+
+    for i in range(number_of_cities-1):
+        last_visited = route[-1]
+        next_location = np.argmin(ma.masked_array(distances[last_visited], mask=mask))
+        route.append(next_location)
+        mask[next_location] = True
+        cost += distances[last_visited, next_location]
+        if i == (number_of_cities - 2):
+            cost += distances[next_location, 0]
+
+    route.insert(len(route), route[0])
+    return route, cost

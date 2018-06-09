f8c0b8f49ae223b89a97bc91f7f7f34d2cd939f6
mathe0 <matty.szczesny@gmail.com>
2018-06-09 15:29:43 +0200

    trace on plot + code clean up

--- tsp_solver/tsp.py
+++ tsp_solver/tsp.py
@@ -1,40 +1,59 @@
 from numpy import random
 import matplotlib.pyplot as plt
 import time
-from tsp_utils import generate_routes, calculate_costs
+from tsp_utils import generate_routes, calculate_costs, points_for_route
 
-NUMBER_OF_CITIES = 8
+NUMBER_OF_CITIES = 5
 DIMENSIONS = 2
 MAXIMUM_DISTANCE = 1000
 
 
 def main():
     cities = list()
-    citiesX, citiesY = random.randint(MAXIMUM_DISTANCE, size = (DIMENSIONS, NUMBER_OF_CITIES))
-    for city_number in range(NUMBER_OF_CITIES) :
-        cities.append([citiesX[city_number], citiesY[city_number]])
+    cities_x, cities_y = random.randint(MAXIMUM_DISTANCE, size=(DIMENSIONS, NUMBER_OF_CITIES))
+    for city_number in range(NUMBER_OF_CITIES):
+        cities.append([cities_x[city_number], cities_y[city_number]])
 
     routes = generate_routes(NUMBER_OF_CITIES)
 
     brute_force_method(cities, routes)
 
-    plt.scatter(*[citiesX, citiesY])
-    for i in range(NUMBER_OF_CITIES) :
-        plt.annotate(str(i), (citiesX[i], citiesY[i]))
-    plt.show()
 
-
-def brute_force_method(cities, routes) :
+def brute_force_method(cities, routes):
     start_time = time.time()
     results = calculate_costs(routes, cities)
     execution_time = time.time() - start_time
 
+    best_route = routes[min(results, key=results.get)]
     print(
         "### BRUTE FORCE METHOD ###" +
-        "route: " + " -> ".join(str(x) for x in routes[min(results, key=results.get)]) +
+        "\nroute: " + " -> ".join(str(x) for x in best_route) +
         "\ndistance: " + str(results[min(results, key=results.get)]) +
         "\ntime: " + str(execution_time) + " seconds"
     )
+
+    cities_x, cities_y = points_for_route(best_route, cities)
+
+    plt.title('BRUTE FORCE')
+
+    plt.plot(cities_x, cities_y, 'o', color='gray', markersize=20,
+             markerfacecolor='white',
+             markeredgecolor='gray',
+             markeredgewidth=1)
+
+    for i in range(len(best_route) - 1):
+        plt.annotate(str(best_route[i]),
+                     (cities_x[i], cities_y[i]),
+                     horizontalalignment='center',
+                     verticalalignment='center',
+                     size=10)
+        plt.annotate("",
+                     xytext=(cities_x[best_route[i]], cities_y[best_route[i]]), textcoords='data',
+                     xy=(cities_x[best_route[i]+1], cities_y[best_route[i]+1]), xycoords='data',
+                     arrowprops=dict(arrowstyle="->",
+                                     connectionstyle="arc3"))
+
+    plt.show()
 
 
 if __name__ == "__main__":
--- tsp_solver/tsp_utils.py
+++ tsp_solver/tsp_utils.py
@@ -2,31 +2,40 @@ import itertools
 import math
 
 
-def count_cost(p1, p2) :
+def count_cost(p1, p2):
     dx = p1[0] - p2[0]
     dy = p1[1] - p2[1]
     return math.hypot(dx, dy)
 
 
-def generate_routes(length) :
-    lang = [ x for x in range(length) ]
+def generate_routes(length):
+    lang = [x for x in range(length)]
     routes = list(map(list, itertools.permutations(lang)))
 
     # insert starting point as a finish point
-    for route in routes :
+    for route in routes:
         route.insert(len(route), route[0])
     return routes
 
 
-def calculate_costs(routes, nodes) :
+def calculate_costs(routes, nodes):
     costs = {}
     for route in routes:
         travel_cost = 0
         for i in range(len(route) - 2):
             move_cost = count_cost(nodes[route[i]], nodes[route[i+1]])
-            print(str(nodes[route[i]]) + "  " + str(nodes[route[i+1]]))
-            print(move_cost)
             travel_cost += move_cost
         costs[routes.index(route)] = travel_cost
 
     return costs
+
+
+def points_for_route(route, cities):
+    x = list()
+    y = list()
+
+    for node in route:
+        x.append(cities[node][0])
+        y.append(cities[node][1])
+
+    return x, y
